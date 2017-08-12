41317c33101f0a896ad4598975383045561ff26b
LPW <lpw@LABS.GARANHUNS.IFPE>
2017-08-12 15:00:59 -0300

    sync with remote

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { FormsModule } from '@angular/forms';
+
 import { AppComponent } from './app.component';
+import { MeuNovoComponent } from "app/meu-novo-component/meu-novo.component";
+import { OlaMundoComponent } from './ola-mundo/ola-mundo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MeuNovoComponent,
+    OlaMundoComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
